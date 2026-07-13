60f87e3ba71785fcad432813d16f5fc8d43c6886
Pono jan <photorealbiz@gmail.com>
2026-07-13 06:12:20 +0330

    Clear input after adding task script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const input = document.getElementById("task-input");
-
 const button = document.getElementById("add-btn");
-
 const list = document.getElementById("task-list");
 
-button.addEventListener("click",function(){
+button.addEventListener("click", function () {
 
-    const li=document.createElement("li");
+    const li = document.createElement("li");
 
-    li.textContent=input.value;
+    li.textContent = input.value;
 
     list.appendChild(li);
+
+    input.value = "";
 
 });
