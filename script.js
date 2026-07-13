bdb87824b3ddc1fad5f43625b1cfe89906c6b728
Pono jan <photorealbiz@gmail.com>
2026-07-13 06:08:24 +0330

    Read task input script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("To-Do List Loaded");
+const input = document.getElementById("task-input");
+
+const button = document.getElementById("add-btn");
+
+button.addEventListener("click",function(){
+
+    console.log(input.value);
+
+});
