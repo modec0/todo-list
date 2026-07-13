ed1aa3f13e3b99073bf300f60b8c6f458dda2a5b
Pono jan <photorealbiz@gmail.com>
2026-07-13 06:09:18 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-console.log("To-Do List Loaded");
 const input = document.getElementById("task-input");
 
 const button = document.getElementById("add-btn");
 
+const list = document.getElementById("task-list");
+
 button.addEventListener("click",function(){
 
-    console.log(input.value);
+    const li=document.createElement("li");
+
+    li.textContent=input.value;
+
+    list.appendChild(li);
 
 });
